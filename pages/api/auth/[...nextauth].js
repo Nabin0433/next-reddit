0047c6f668b0afca12bdd4a3e58eddd19ad88d93
nabin <nabin043@gmail.com>
2022-06-01 22:33:40 +0545

    fix-reddit-and-secret

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -4,9 +4,8 @@ import RedditProvider from 'next-auth/providers/reddit'
 export default NextAuth({
   providers: [
     RedditProvider({
-      clientId: 'K0bMvC5xZkOa_kCfUmDag',
-      clientSecret: '2-YFAtYU-7kXHIw1-1tO-N6XjVCoGA',
+      clientId: process.env.REDDIT_CLIENT_ID,
+      clientSecret: process.env.REDDIT_CLIENT_SECRET,
     }),
   ],
-  secret: process.env.NEXTAUTH_MY_APP_SECRET,
 })
