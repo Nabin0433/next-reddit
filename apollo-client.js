35fdfed14a088d7c351ebdbb85f37b395ce94336
nabin <nabin043@gmail.com>
2022-06-01 18:53:53 +0545

    live-deploy

--- apollo-client.js
+++ apollo-client.js
@@ -3,7 +3,7 @@ import { ApolloClient, InMemoryCache } from '@apollo/client'
 const client = new ApolloClient({
   uri: 'https://walledlake.stepzen.net/api/coiled-stingray/__graphql',
   headers: {
-    authorization: `ApiKey walledlake::stepzen.net+1000::4a32b01ef93130ea92e54b5862fc97de4cce6ffa60b66a4091644958cb7c0b5d`,
+    authorization: `ApiKey ${process.env.NEXT_PUBLIC_STEPZEN_KEY}`,
   },
   cache: new InMemoryCache(),
 })
