627c66f12509030d367a7e198cf7c2131970ef68
nabin <nabin043@gmail.com>
2022-06-01 22:18:47 +0545

    live-deploy

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -4,8 +4,9 @@ import RedditProvider from 'next-auth/providers/reddit'
 export default NextAuth({
   providers: [
     RedditProvider({
-      clientId: process.env.REDDIT_CLIENT_ID,
-      clientSecret: process.env.REDDIT_CLIENT_SECRET,
+      clientId: 'K0bMvC5xZkOa_kCfUmDag',
+      clientSecret: '2-YFAtYU-7kXHIw1-1tO-N6XjVCoGA',
     }),
   ],
+  secret: process.env.NEXTAUTH_MY_APP_SECRET,
 })
